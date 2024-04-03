aed48b0ef0ca7c5c8db2a5c8637a603209593dd8
zxcfer <zxcfer@proton.me>
2024-04-03 12:31:53 +0200

    google auth removd

--- client/src/providers/torus.tsx
+++ client/src/providers/torus.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
 import Torus from "@toruslabs/torus.js";
 import NodeDetailsManager from "@toruslabs/fetch-node-details";
-import nacl from "tweetnacl";
 import { Keypair } from "@solana/web3.js";
-import {
-  useGoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
-
 import { reportError } from "utils";
 
 const origin = window.location.origin;
@@ -58,51 +51,9 @@ const StateContext = React.createContext<State | undefined>(undefined);
 type Props = { children: React.ReactNode };
 export function TorusProvider({ children }: Props) {
   const [fetchStatus, setFetchStatus] = React.useState<FetchStatus>();
-  const [googleResponse, setGoogleResponse] =
-    React.useState<GoogleLoginResponse>();
   const [nodeDetails, setNodeDetails] = React.useState<NodeDetails>();
   const [wallet, setWallet] = React.useState<Keypair>();
   const [error, setError] = React.useState<string>();
-
-  const responseGoogle = React.useCallback(
-    async (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-      if (!("code" in response)) {
-        setGoogleResponse(response);
-      }
-    },
-    []
-  );
-
-  const { signIn, loaded } = useGoogleLogin({
-    clientId: USE_TORUS_TESTNET ? TEST_CLIENT_ID : CLIENT_ID,
-    onSuccess: responseGoogle,
-    onFailure: (err) => {
-      if (!ENABLE_TORUS) return;
-      reportError(err, "Google login failed");
-      setFetchStatus("failure");
-      setError("Failed to login");
-    },
-    isSignedIn: ENABLE_TORUS,
-  });
-
-  const disconnect = React.useCallback(() => {
-    if (!googleResponse) return;
-    googleResponse.disconnect();
-    setGoogleResponse(undefined);
-  }, [googleResponse]);
-
-  const connect = React.useCallback(
-    (mode: ConnectionMode) => {
-      if (mode === "fresh") {
-        setFetchStatus("fetching");
-        disconnect();
-        signIn();
-      } else if (mode === "cached") {
-        setFetchStatus("reloading");
-      }
-    },
-    [disconnect, signIn]
-  );
 
   React.useEffect(() => {
     if (!ENABLE_TORUS) return;
@@ -123,7 +74,6 @@ export function TorusProvider({ children }: Props) {
 
   // Detects when a user has signed in and fetches the user's Torus private key
   React.useEffect(() => {
-    if (!nodeDetails || !googleResponse) return;
 
     let connectionMode: ConnectionMode;
     if (fetchStatus === "reloading") {
@@ -137,65 +87,13 @@ export function TorusProvider({ children }: Props) {
     let unmounted = false;
     (async () => {
       const torus = new Torus({});
-      const { torusNodeEndpoints, torusNodePub, torusIndexes } = nodeDetails;
-
-      try {
-        const verifierId = googleResponse.getBasicProfile().getEmail();
-
-        // Creates a new key for the verifierId if it doesn't exist yet
-        await torus.getPublicAddress(
-          torusNodeEndpoints,
-          torusNodePub,
-          { verifier: VERIFIER, verifierId },
-          false
-        );
-
-        let idToken = googleResponse.getAuthResponse().id_token;
-        if (connectionMode === "cached") {
-          idToken = (await googleResponse.reloadAuthResponse()).id_token;
-        }
-
-        const { privKey } = await torus.retrieveShares(
-          torusNodeEndpoints,
-          torusIndexes,
-          VERIFIER,
-          { verifier_id: verifierId },
-          idToken
-        );
-        if (unmounted) return;
-        const torusKey = Buffer.from(privKey.toString(), "hex");
-        const keyPair = nacl.sign.keyPair.fromSeed(torusKey);
-        setWallet(Keypair.fromSecretKey(keyPair.secretKey));
-        setFetchStatus("success");
-      } catch (err) {
-        reportError(err, "failed to fetch torus key");
-        setFetchStatus("failure");
-        setError("Failed to fetch Torus key");
-      }
     })();
 
     return () => {
       unmounted = true;
     };
-  }, [nodeDetails, googleResponse, fetchStatus]);
+  }, [nodeDetails, fetchStatus]);
 
-  const state = React.useMemo(
-    () => ({
-      enabled: ENABLE_TORUS,
-      loaded: loaded || !ENABLE_TORUS,
-      loadingWallet: fetchStatus === "fetching" || fetchStatus === "reloading",
-      error,
-      wallet,
-      email: googleResponse?.getBasicProfile().getEmail(),
-      connect,
-      disconnect,
-    }),
-    [loaded, fetchStatus, error, wallet, googleResponse, connect, disconnect]
-  );
-
-  return (
-    <StateContext.Provider value={state}>{children}</StateContext.Provider>
-  );
 }
 
 export function useTorusState(): State {
