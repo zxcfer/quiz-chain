764ce3768b8bd03cd4a7a341388ead6ea9feb9d3
zxcfer <zxcfer@proton.me>
2024-04-08 12:19:56 +0200

    fix #2 ─ ChartData issue fix

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,6 +5,7 @@ module.exports = function override(config) {
 	  Object.assign(fallback, {
 		crypto: require.resolve("crypto-browserify"),
 		stream: require.resolve("stream-browserify"),
+		vm: require.resolve("vm-browserify"),
 		assert: false, // require.resolve("assert") can be polyfilled here if needed
 		http: false, // require.resolve("stream-http") can be polyfilled here if needed
 		https: false, // require.resolve("https-browserify") can be polyfilled here if needed
